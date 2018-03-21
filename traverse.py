172b661c6b30f2d6a83f7de3b1a263c38f4570cc
Alphadose <anish.mukherjee1996@gmail.com>
2018-03-22 04:20:49 +0530

    Shifted to python runtime

--- traverse.py
+++ traverse.py
@@ -1,18 +1,17 @@
-import sys, subprocess
+from antlr4 import *
+from JavaLexer import JavaLexer
+from JavaParser import JavaParser
+from JavaParserListener import JavaParserListener
+import sys
 
-grammar_name = sys.argv[1]
-grammar_rule = sys.argv[2]
-file_path = sys.argv[3]
+def main():
+    lexer = JavaLexer(FileStream(sys.argv[1]))
+    stream = CommonTokenStream(lexer)
+    parser = JavaParser(stream)
+    tree = parser.compilationUnit()
+    printer = JavaParserListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
 
-with open(file_path) as f:
-	file = f.read()
-
-subprocess.call('java -jar /usr/local/lib/antlr-4.7.1-complete.jar {}Lexer.g4'.format(grammar_name), shell=True)
-
-subprocess.call('java -jar /usr/local/lib/antlr-4.7.1-complete.jar {}Parser.g4'.format(grammar_name), shell=True)
-
-subprocess.call('/usr/bin/javac {}*.java'.format(grammar_name), shell=True)
-
-out = subprocess.check_output('echo \'{}\' | java org.antlr.v4.gui.TestRig {} {} -tree'.format(file, grammar_name, grammar_rule), shell=True)
-
-print ("AST output:\n" + out)
+if __name__ == '__main__':
+    main()
